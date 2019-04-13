63013b379f0bb76ae74ae2cfabebfce116a042e5
BuildTools <unconfigured@null.spigotmc.org>
2019-04-13 18:54:03 +0200

    nieuwe geluiden

--- app/src/main/java/com/thoughnut/soundboard/Utils.java
+++ app/src/main/java/com/thoughnut/soundboard/Utils.java
@@ -5,5 +5,4 @@ public class Utils {
     public static final String darrie = "https://raw.githubusercontent.com/Thoughnut/Arrieboard/master/app/src/main/res/raw/darrie.zip";
     public static final String djappie = "https://raw.githubusercontent.com/Thoughnut/Arrieboard/master/app/src/main/res/raw/djappie.zip";
     public static final String drest = "https://raw.githubusercontent.com/Thoughnut/Arrieboard/master/app/src/main/res/raw/drest.zip";
-
 }
